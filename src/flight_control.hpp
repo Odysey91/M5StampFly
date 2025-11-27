cc529d5157a6ba4f981ce2774c02bbeb277c0c30
JetLotusS <64372194+JetLotusS@users.noreply.github.com>
2025-11-27 11:27:50 +0100

    Add MISSION_MODE constant to flight control

--- src/flight_control.hpp
+++ src/flight_control.hpp
@@ -43,6 +43,7 @@
 #define LOG_MODE          4
 #define AUTO_LANDING_MODE 5
 #define FLIP_MODE         6
+#define MISSION_MODE      7
 
 #define POWER_LIMIT         3.34
 #define UNDER_VOLTAGE_COUNT 100
