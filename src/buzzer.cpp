fb62fb7135748f168c9e5dd7b01c96a99d1a1f58
JetLotusS <64372194+JetLotusS@users.noreply.github.com>
2025-11-27 09:45:38 +0100

    Add 'songs_of_storms_tone_1_oct_d_short' function

--- src/buzzer.cpp
+++ src/buzzer.cpp
@@ -29,9 +29,60 @@ void beep(void) {
     buzzer_sound(4000, 100);
 }
 
-void start_tone(void) {
+void songs_of_storms_tone_1_oct_d_short(void) {
+    struct {
+        uint32_t freq;
+        uint32_t duration;
+    } notes[] = {
+
+        {293, 180},   // D4
+        {349, 180},   // F4
+        {587, 450},   // D5
+
+        {293, 180},   // D4
+        {349, 180},   // F4
+        {587, 450},   // D5
+
+        {659, 400},   // E5
+        {698, 200},   // F5
+        {659, 180},   // E5
+        {698, 180},   // F5
+        {659, 180},   // E5
+        {523, 180},   // C5
+        {440, 400},   // A4
+
+        {440, 300},   // A4
+        {293, 300},   // D4
+        {349, 200},   // F4
+        {392, 200},   // G4
+        {440, 600},   // A4
+
+        {440, 300},   // A4
+        {293, 300},   // D4
+        {349, 200},   // F4
+        {392, 200},   // G4
+        {330, 600},   // E4
+    };
+
+    int num_notes = sizeof(notes) / sizeof(notes[0]);
+    
+    for (int i = 0; i < num_notes; i++) {
+        if (notes[i].freq == 0) {
+            // Rest: just delay
+            vTaskDelay(notes[i].duration / portTICK_PERIOD_MS);
+        } else {
+            buzzer_sound(notes[i].freq, notes[i].duration);
+        }
+    }
+}
+
+void default_tone(void) {
     buzzer_sound(NOTE_D1, 200);
     buzzer_sound(NOTE_D5, 200);
     buzzer_sound(NOTE_D3, 200);
     buzzer_sound(NOTE_D4, 200);
+}
+void start_tone(void) {
+    //default_tone();
+    songs_of_storms_tone_1_oct_d_short();
 }
